14e5781d80393ffbd05b650f7fc2b451b8a80b01
Joakim-Animate90 <joakimbwire23@gmail.com>
2022-11-19 12:24:21 +0300

    Add password field class

---
+++ boomify_app/lib/utils/password_field.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PassWordField extends StatelessWidget {
+  const PassWordField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofocus: false,
+      initialValue: 'some password',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+  }
+}
