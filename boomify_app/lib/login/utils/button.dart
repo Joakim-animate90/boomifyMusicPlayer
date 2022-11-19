d1554c52ea4703881afdf2542af7190e9ff7d736
Joakim-Animate90 <joakimbwire23@gmail.com>
2022-11-19 12:57:24 +0300

    updated button widget

---
+++ boomify_app/lib/login/utils/button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.green),
+            padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
+            textStyle: MaterialStateProperty.all(
+                const TextStyle(fontSize: 14, color: Colors.lightBlueAccent))),
+        onPressed: () {},
+        child: const Text('Log In', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
